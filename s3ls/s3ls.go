0b01ec713b8e77aff0e53e35819e022c488d89be
Don Mills <dmills@singlestoneconsulting.com>
2016-05-10 16:13:46 -0400

    Changed for new AWS-SDK

--- s3ls/s3ls.go
+++ s3ls/s3ls.go
@@ -2,47 +2,43 @@ package main
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	//  "github.com/aws/aws-sdk-go/aws/awsutil"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"os"
-  "sort"
+	"sort"
 )
 
 type s3items struct {
-  Name string
-  Size int64
+	Name string
+	Size int64
 }
 type results []s3items
 
 func (slice results) Len() int {
-    return len(slice)
+	return len(slice)
 }
 
 func (slice results) Less(i, j int) bool {
-    return slice[i].Size < slice[j].Size;
+	return slice[i].Size < slice[j].Size
 }
 
 func (slice results) Swap(i, j int) {
-    slice[i], slice[j] = slice[j], slice[i]
+	slice[i], slice[j] = slice[j], slice[i]
 }
 
-
 func main() {
-  bucket := ""
-  region := ""
-  if len(os.Args) == 1  {
-    fmt.Println("Usage: s3ls {bucketname} [region]\nError: Missing Bucket Name")
-    os.Exit(1)
-  } else {
-    bucket = os.Args[1]
-    if len(os.Args) == 3 {
-      region = os.Args[2]
-    } else {
-      region = "us-east-1"
-    }
-  }
-	svc := s3.New(&aws.Config{Region: aws.String(region)})
+	bucket := ""
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: s3ls {bucketname}\nError: Missing Bucket Name")
+		os.Exit(1)
+	} else {
+		bucket = os.Args[1]
+	}
+
+	svc := s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
 	params := &s3.ListObjectsInput{
 		Bucket: aws.String(bucket), // Required
 	}
@@ -63,24 +59,19 @@ func main() {
 		}
 	}
 
-
 	// Pretty-print the response data.
-  var list results
-	for e := range resp.Contents {
+	var list results
+	for e := 0; e < len(resp.Contents); e++ {
 		if *resp.Contents[e].Size != 0 {
-      var temp s3items
-      temp.Name, temp.Size = *resp.Contents[e].Key, *resp.Contents[e].Size
-      list = append(list,temp)
+			var temp s3items
+			temp.Name, temp.Size = *resp.Contents[e].Key, *resp.Contents[e].Size
+			list = append(list, temp)
 			//fmt.Println(*resp.Contents[e].Size, *resp.Contents[e].Key)
 		}
 	}
-  sort.Sort(list)
-  var totalsize int64
-  for e:= range list {
-    totalsize = totalsize + list[e].Size
-    //fmt.Println(list[e].Size,list[e].Name)
-    fmt.Printf("%-15d%s\n", list[e].Size,list[e].Name)
-  }
-  fmt.Printf("Bucket has %d items.\n", len(list))
-  fmt.Printf("%.2f Megabytes total space used.\n", float64(totalsize)/1000000)
+	sort.Sort(list)
+	for e := 0; e < len(list); e++ {
+		fmt.Println(list[e].Size, list[e].Name)
+	}
+	fmt.Printf("Bucket has %d items\n", len(list))
 }
